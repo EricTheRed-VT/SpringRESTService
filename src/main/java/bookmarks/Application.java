259b98188209c7ba0a8f13dc9e98600e4b6136f5
Eric <ericrainvillevt@gmail.com>
2017-06-28 22:42:38 -0400

    implementing HATEOAS compliance

--- src/main/java/bookmarks/Application.java
+++ src/main/java/bookmarks/Application.java
@@ -25,12 +25,12 @@ public class Application {
     @Bean
     CommandLineRunner init(AccountRepository accountRepository,
                            BookmarkRepository bookmarkRepository) {
-        return (evt) -> Arrays.asList("jim,bob,joe,tom,wolfgang".split(","))
+        return (args) -> Arrays.asList("jim,bob,joe,tom,wolfgang".split(","))
                         .forEach(
                                 a -> {
                                     Account account = accountRepository.save(new Account(a, "password"));
                                     bookmarkRepository.save(new Bookmark(account, "http://bookmark.com/1/" + a, "A description"));
-                                    bookmarkRepository.save(new Bookmark(account, "http://bookmark.com/2/" + a, "Another description"));
+                                    bookmarkRepository.save(new Bookmark(account, "http://bookmark.com/2/" + a, "A description"));
                                 }
                         );
     }
--- src/main/java/bookmarks/BookmarkRestController.java
+++ src/main/java/bookmarks/BookmarkRestController.java
@@ -1,12 +1,16 @@
 package bookmarks;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resources;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by eric on 6/28/17.
@@ -19,7 +23,6 @@ class BookmarkRestController {
     private final BookmarkRepository bookmarkRepository;
     private final AccountRepository  accountRepository;
 
-    @Autowired
     BookmarkRestController(BookmarkRepository br,
                            AccountRepository  ar ) {
         this.bookmarkRepository = br;
@@ -27,38 +30,40 @@ class BookmarkRestController {
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    Collection<Bookmark> readBookmarks(@PathVariable String userId) {
+    Resources<BookmarkResource> readBookmarks(@PathVariable String userId) {
         this.validateUser(userId);
-        return this.bookmarkRepository.findByAccountUsername(userId);
+
+        List<BookmarkResource> bookmarkResourceList = bookmarkRepository
+                                                        .findByAccountUsername(userId)
+                                                        .stream()
+                                                        .map(BookmarkResource::new)
+                                                        .collect(Collectors.toList());
+
+        return new Resources<>(bookmarkResourceList);
     }
 
     @RequestMapping(method = RequestMethod.POST)
     ResponseEntity<?> add(@PathVariable String userId, @RequestBody Bookmark input) {
         this.validateUser(userId);
-        return this.accountRepository
-                        .findByUsername(userId)
-                        .map(account -> {
-                            Bookmark result = bookmarkRepository.save(new Bookmark(account, input.uri, input.description));
-
-                            URI location = ServletUriComponentsBuilder
-                                                .fromCurrentRequest()
-                                                .path("/{id}")
-                                                .buildAndExpand(result.getId())
-                                                .toUri();
-                            return ResponseEntity.created(location).build();
-                        })
-                        .orElse(ResponseEntity.noContent().build());
+        return accountRepository
+                .findByUsername(userId)
+                .map(account -> {
+                    Bookmark bookmark = bookmarkRepository.save(new Bookmark(account, input.uri, input.description));
+                    Link forOneBookmark = new BookmarkResource(bookmark).getLink("self");
+                    return ResponseEntity.created(URI.create(forOneBookmark.getHref())).build();
+                })
+                .orElse(ResponseEntity.noContent().build());
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/{bookmarkId}")
-    Bookmark readBookmark(@PathVariable String userId, @PathVariable Long bookmarkId) {
+    BookmarkResource readBookmark(@PathVariable String userId, @PathVariable Long bookmarkId) {
         this.validateUser(userId);
-        return this.bookmarkRepository.findOne(bookmarkId);
+        return new BookmarkResource(this.bookmarkRepository.findOne(bookmarkId));
     }
 
     private void validateUser(String userId) {
-        this.accountRepository.findByUsername(userId).orElseThrow(
-                () -> new UserNotFoundException(userId)
-        );
+        this.accountRepository
+                .findByUsername(userId)
+                .orElseThrow(() -> new UserNotFoundException(userId));
     }
 }
